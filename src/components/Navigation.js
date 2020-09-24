3ef5eee2105e026be44ce9d202c07a491dfc424a
DDDieon <59174247+DDDieon@users.noreply.github.com>
2020-09-24 22:07:42 +0900

    Set Auth form Google, Github OAuth2.0

---
+++ src/components/Navigation.js
@@ -0,0 +1,4 @@
+import React from "react"
+
+const Navigation = () => <nav></nav>
+export default Navigation
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,4 +1,4 @@
-import { authService } from "fbase"
+import { authService, firebaseInstance } from "fbase"
 import React, { useState } from "react"
 
 const Auth = () => {
@@ -35,6 +35,19 @@ const Auth = () => {
         // 이 함수는 newAccount(계정존재여부)를 토글한다.
         setNewAccount((prev) => !prev)
     }
+    const onSocialClick = async (event) => {
+        const {
+            target: { name },
+        } = event
+        let provider
+        if (name === "google") {
+            provider = new firebaseInstance.auth.GoogleAuthProvider()
+        } else if (name === "github") {
+            provider = new firebaseInstance.auth.GithubAuthProvider()
+        }
+        const data = await authService.signInWithPopup(provider)
+        console.log(data)
+    }
     return (
         <div>
             <form onSubmit={onSubmit}>
@@ -59,8 +72,12 @@ const Auth = () => {
             </form>
             <span onClick={toggleAccount}>테스트용</span>
             <div>
-                <button>Continue with Google</button>
-                <button>Continue with Github</button>
+                <button onClick={onSocialClick} name="google">
+                    Continue with Google
+                </button>
+                <button onClick={onSocialClick} name="github">
+                    Continue with Github
+                </button>
             </div>
         </div>
     )
